59717aae382e120e87a5e7e61c8f56c8c3b3d1b9
Nathan <nathan.titirangi@gmail.com>
2025-04-12 17:07:54 +1200

    make the colouring less dumb iterators

--- src/main.rs
+++ src/main.rs
@@ -6,12 +6,12 @@ use std::{
 };
 
 #[derive(PartialEq, Eq, Copy, Clone, Hash, Debug, Default)]
-struct ColourKey {
-    idx: usize,
+pub struct ColourKey {
+    pub idx: usize,
 }
 
 const RESET: &'static str = "\x1B[0m";
-trait Colour: Clone {
+pub trait Colour: Clone {
     fn ansify(&self) -> String;
 }
 
@@ -45,72 +45,14 @@ impl RGB {
     }
 }
 
-#[derive(Copy, Clone)]
-pub struct DiscordColour {
-    value: u8,
+fn to_discord_colour(key: ColourKey) -> String {
+    "\x1B[".to_string() + &((key.idx % 7 + 31) as u8).to_string() + "m"
 }
 
-impl Colour for DiscordColour {
-    fn ansify(&self) -> String {
-        "\x1B[".to_string() + &self.value.to_string() + "m"
-    }
-}
-
-pub struct DiscordColourIterator {
-    index: usize,
-    total: usize,
-}
-
-impl DiscordColourIterator {
-    pub fn new(total: usize) -> Self {
-        Self { index: 0, total }
-    }
-}
-
-impl Iterator for DiscordColourIterator {
-    type Item = DiscordColour;
-
-    fn next(&mut self) -> Option<Self::Item> {
-        if self.index >= self.total {
-            return None;
-        }
-        self.index += 1;
-
-        let remainder = self.index % 8;
-        // TODO: consider bg combos
-
-        Some(DiscordColour {
-            value: remainder as u8 + 30,
-        })
-    }
-}
-
-pub struct RGBIterator {
-    index: usize,
-    total: usize,
-}
-
-impl RGBIterator {
-    pub fn new(total: usize) -> Self {
-        Self { index: 0, total }
-    }
-}
-
-impl Iterator for RGBIterator {
-    type Item = RGB;
-
-    fn next(&mut self) -> Option<Self::Item> {
-        if self.index >= self.total {
-            return None;
-        }
-
-        let golden_angle = (1.0 + f64::sqrt(5.0)) * 60.0;
-        let hue = (self.index as f64 * golden_angle) % 360.0;
-
-        self.index += 1;
-
-        Some(RGB::from_hsl(hue, 0.9, 0.6))
-    }
+fn to_rgb_colour(key: ColourKey) -> String {
+    let angle = (1.0 + f64::sqrt(5.0)) * 60.0;
+    let hue = (key.idx as f64 * angle) % 360.0;
+    RGB::from_hsl(hue, 0.9, 0.6).ansify()
 }
 
 impl ColourKey {
@@ -124,16 +66,16 @@ impl ColourKey {
 }
 
 /// Like a [`char`], if `T` is [`PossiblyIsomorphic`] then it must be possible to build part of a bijective function `T -> T` out of it (this is the isomorphism)
-trait PossiblyIsomorphic: Clone + Eq + Hash {}
+pub trait PossiblyIsomorphic: Clone + Eq + Hash {}
 impl<T: Clone + Eq + Hash> PossiblyIsomorphic for T {}
 
 /// An object is a valid response if it can do some basic stuff, this trait is effectively a trait alias
-trait Response: Clone {}
+pub trait Response: Clone {}
 impl<T: Clone> Response for T {}
 
 /// Like a [`String`], `R` (the response) is the value which is used to identify this if it was isomorphic
 /// For a string where you wanted to find isomorphisms in overlapping windows you might have `T` = [`char`] and `R` = [`std::ops::Range`]
-struct IsomorphicHolder<T: PossiblyIsomorphic, R, I: Iterator<Item = T>> {
+pub struct IsomorphicHolder<T: PossiblyIsomorphic, R, I: Iterator<Item = T>> {
     iter: I,
     response: R,
 }
@@ -141,15 +83,15 @@ struct IsomorphicHolder<T: PossiblyIsomorphic, R, I: Iterator<Item = T>> {
 /// An object which holds a response possibly coloured by it's isomorphisms
 /// If you just want to display it make sure `R`: [`ToString`] and use `ansify` on it
 #[derive(Clone, Debug)]
-struct ColouredObject<R: Response> {
+pub struct ColouredObject<R: Response> {
     value: R,
     colour: Option<ColourKey>,
 }
 
 impl<R: Response + ToString> ColouredObject<R> {
-    pub fn ansify<U: Colour>(&self, table: &[U]) -> String {
+    pub fn ansify(&self, paint: impl Fn(ColourKey) -> String) -> String {
         match self.colour {
-            Some(colour) => colour.reify(table).ansify() + &self.value.to_string(),
+            Some(colour) => paint(colour) + &self.value.to_string(),
             None => self.value.to_string(),
         }
     }
@@ -186,7 +128,7 @@ struct IsomorphManager<T: PossiblyIsomorphic> {
     words: Vec<ColouredObject<T>>,
 }
 
-pub fn colour<
+pub fn colour_isomorphs<
     T: PossiblyIsomorphic,
     R: Response,
     I2: Iterator<Item = T>,
@@ -246,14 +188,13 @@ fn main() -> Result<(), Box<dyn Error>> {
     let mut buf = String::new();
     stdin.read_to_string(&mut buf)?;
 
-    // TODO: cli arg to swap
-    let table = DiscordColourIterator::new(1000).collect::<Vec<_>>(); //RGBIterator::new(1000).collect::<Vec<_>>();
-    let coloured = colour(buf.split(' ').map(|e| IsomorphicHolder {
+    let coloured = colour_isomorphs(buf.split(' ').map(|e| IsomorphicHolder {
         iter: e.chars(),
         response: e,
     }))
     .into_iter()
-    .map(|e| e.ansify(&table))
+    // TODO: cli arg to swap to to_rgb_colour
+    .map(|e| e.ansify(to_discord_colour))
     .map(|e| e + RESET)
     .fold("".to_string(), |acc, e| acc + &e + " ");
 
